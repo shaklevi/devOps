3c6cc23e55936e9d18d4afae0177f8077749ed5f
shaklevi <shak.levi@gmail.com>
2019-10-23 19:52:35 +0300

    added venv

--- InitVMs/aws_installer.py
+++ InitVMs/aws_installer.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
     instances = ec2.create_instances(
         ImageId='ami-08f0c33d35b3c9ddb',
         MinCount=1,
-        MaxCount=2,
+        MaxCount=1,
         KeyName='net4ukey',
-        InstanceType="t2.micro"
+        InstanceType="t2.micro",
+        NetworkInterfaces=[{
+            "DeviceIndex": 0,
+            "SubnetId": 'subnet-0924711b1c1e40fcc',
+            "AssociatePublicIpAddress": True,
+        }]
     )
